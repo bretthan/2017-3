4faa98b83d313b76176d67c2b125004808b91248
bretthan <bretthan@seattleacademy.org>
2020-02-05 11:12:38 -0800

    Add webpage server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+const port = 1509
+let counter = 0
+
+app.use(express.static('public'))
+
+app.get('/hello', (req, res) => res.send('Hello Brett!'))
+
+app.get('/counter', function (req, res) {
+	counter = counter + 1;
+  res.send("Counter " + counter);
+})
+
+app.get('/date', function (req, res) {
+	let d = new Date();
+	console.log(d);
+  res.send("Date " + d);
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
